ddabb7768c64b59a7cd50cc8c4cc60f3df9b7afe
Jason Miesionczek <atmospherian@gmail.com>
2025-09-07 22:49:30 -0400

    complete redesign and refactor of advocate search page

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,12 +1,32 @@
+import { NextRequest } from "next/server";
 import db from "../../../db";
 import { advocates } from "../../../db/schema";
-import { advocateData } from "../../../db/seed/advocates";
+import { arrayContains, arrayOverlaps, eq, ilike, inArray, or, sql, SQL } from "drizzle-orm";
 
-export async function GET() {
-  // Uncomment this line to use a database
-  // const data = await db.select().from(advocates);
+export async function GET(request: NextRequest) {
+  const { searchParams } = new URL(request.url);
+  const searchTerm = searchParams.get("searchTerm") || "";
+  
+  // If a search term is provided, filter the advocates based on it (you can customize the filtering logic as needed)
+  if (searchTerm) {
+    const filters: SQL[] = [
+      ilike(advocates.firstName, `%${searchTerm}%`),
+      ilike(advocates.lastName, `%${searchTerm}%`),
+      ilike(advocates.city, `%${searchTerm}%`),
+      ilike(advocates.degree, `%${searchTerm}%`),
+      //arrayContains(advocates.specialties, [searchTerm]),
+      //arrayOverlaps(advocates.specialties, [searchTerm]),
+    ];
 
-  const data = advocateData;
+    if (!isNaN(Number(searchTerm))) {
+      filters.push(eq(advocates.yearsOfExperience, Number(searchTerm)));
+    }
+
+    const data = await db.select().from(advocates).where(or(...filters));
+    return Response.json({ data });
+  }
+  
+  const data = await db.select().from(advocates);
 
   return Response.json({ data });
 }
